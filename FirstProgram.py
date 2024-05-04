fd26bd61830c4f10bfb02fdb894d8a81c0bd6943
DevAdnan55 <161664463+DevAdnan55@users.noreply.github.com>
2024-05-04 15:31:48 -0400

    Python Codes

---
+++ FirstProgram.py
@@ -0,0 +1,6 @@
+print("Hey, Myself Adnan Shaikh taking first steps towards python");
+print("Hello World", "I'm using Python");
+print("Hello World");
+print("I'm using Python");
+print(55);
+print(55+22);
---
+++ Variable.py
@@ -0,0 +1,9 @@
+name = "Adnan Shaikh"; #string
+age = 23; #Integer Value
+height = 6.1; #Floating Value
+
+# = is an assigment operator
+
+print("my name is :", name);
+print("my age is :", age);
+print("my height is :",height);
