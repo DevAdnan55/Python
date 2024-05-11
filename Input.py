edfd3ea38eceff20aca1bf23343032d4b4ddabac
Adnan shaikh <adnanshaikh218557@gmail.com>
2024-05-11 00:30:45 -0400

    New file called Input

---
+++ Input.py
@@ -0,0 +1,11 @@
+name = input("Enter your name : ");
+age = int(input("Enter your age : "));
+marks = float(input("Enter your marks :"));
+
+print("welcome ", name);
+print("your age is ", age);
+print("marks = ", marks);
+
+print(type(name));
+print(type(age));
+print(type(marks));
