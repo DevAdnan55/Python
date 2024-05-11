494ba12935be456dea4015b6e81e0a722a9752f3
Adnan shaikh <adnanshaikh218557@gmail.com>
2024-05-11 00:53:44 -0400

    Create Pratice_1.py

---
+++ Pratice_1.py
@@ -0,0 +1,10 @@
+#Practice: Write a Program to input 2 numbers & print their sum
+
+first_Number = float(input("Enter first value : "));
+second_Number = float(input("Enter second value : "));
+sum = first_Number + second_Number;
+
+print("your first number is ", first_Number);
+print("your second number is ", second_Number);
+print("Adding both the numbers");
+print("Answer : ", sum);
